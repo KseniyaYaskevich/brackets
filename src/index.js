0f0e1c5296670f1e986a6a7df7781537eec24e5f
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-02-06 11:50:48 +0300

    feat: feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const brackets = str.split('');
+  const openBrackets = bracketsConfig.map(bracket => bracket[0]);
+  const bracketsPairs = Object.fromEntries(bracketsConfig);
+  let stack = [];
+
+  for (let bracket of brackets) {
+    if (openBrackets.includes(bracket)) {
+      if (bracketsPairs[bracket] != bracket) {
+        stack.push(bracket);
+      }
+      if (bracketsPairs[bracket] === bracket) {
+        if (!stack.includes(bracket)) {
+          stack.push(bracket);
+        } else if (bracket === stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    } else if (bracket === bracketsPairs[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
